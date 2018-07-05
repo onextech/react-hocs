8045e573ad945d3e548e655286d725eebd3a54f8
clodal <joel.koh@me.com>
2018-07-06 03:03:51 +0800

    added css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /(node_modules|bower_components)/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   externals: {
